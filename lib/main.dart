906d7fe9c47d13bf49d171d2cee6637d7003b837
chetansinghrajput <chetanrajput076@gmail.com>
2024-02-19 10:53:04 +0530

    intial changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
 void main(){
-  
+  runApp(MaterialApp(
+    title: "learning App",
+    home:HomePage(),
+  ) );
+}
+
+class HomePage   extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("learning App"),
+      ),
+   
+    );
+  }
 }
